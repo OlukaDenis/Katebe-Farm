03d89b4ed1a4bffcea90206b18a84c6e509c824d
OlukaDenis <olukadeno@gmail.com>
2020-09-02 15:12:43 -0700

    Add login page

---
+++ src/connector/DbConnection.java
@@ -0,0 +1,25 @@
+
+package connector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DbConnection {
+    private static final String DB_URL = "jdbc:mysql://localhost/katebe";
+    
+    public static Connection getConnection() {
+        Connection con = null;
+        
+        try {        
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(DB_URL, "root","");
+        } catch (Exception ex) {
+           System.out.println(ex.getMessage());
+        }
+        
+        return con;
+    }
+    
+}
